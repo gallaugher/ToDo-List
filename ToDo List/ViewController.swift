8541f250514a5f6b79849648e7a38add1fb9397b
John Gallaugher <john.gallaugher@bc.edu>
2020-01-30 17:41:51 -0500

    Initial Commit

---
+++ ToDo List/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDo List
+//
+//  Created by John Gallaugher on 1/30/20.
+//  Copyright © 2020 John Gallaugher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
