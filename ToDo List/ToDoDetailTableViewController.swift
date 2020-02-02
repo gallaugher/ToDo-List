916171dc0a17b3fd4c76511ce17b0258ddbb43cd
John Gallaugher <john.gallaugher@bc.edu>
2020-02-02 18:05:09 -0500

    Fixing stuff

---
+++ ToDo List/ToDoDetailTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ToDoDetailTableViewController.swift
+//  ToDo List
+//
+//  Created by John Gallaugher on 2/1/20.
+//  Copyright © 2020 John Gallaugher. All rights reserved.
+//
+
+import UIKit
+
+class ToDoDetailTableViewController: UITableViewController {
+    @IBOutlet weak var saveBarButton: UIBarButtonItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func cancelButtonPressed(_ sender: UIBarButtonItem) {
+    }
+}
---
+++ ToDo List/ToDoListViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  ToDo List
+//
+//  Created by John Gallaugher on 1/30/20.
+//  Copyright © 2020 John Gallaugher. All rights reserved.
+//
+
+import UIKit
+
+class ToDoListViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+    
+    var toDoArray = ["Learn Swift", "Build Apps", "Change the World", "Take a Vacation"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+}
+
+extension ToDoListViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print("😎 numberOfRowsInSection was just called. Returning \(toDoArray.count)")
+        return toDoArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        print("🚣‍♂️ cellForRowAt was just called for indexPath.row = \(indexPath.row) which is the cell containing \(toDoArray[indexPath.row])")
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = toDoArray[indexPath.row]
+        return cell
+    }
+}
