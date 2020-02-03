7d0b70d36f7e208ab70479502c6d451ff0e54342
John Gallaugher <john.gallaugher@bc.edu>
2020-02-03 07:31:11 -0500

    Complete through 5.3

--- ToDo List/ToDoDetailTableViewController.swift
+++ ToDo List/ToDoDetailTableViewController.swift
@@ -10,11 +10,20 @@ import UIKit
 
 class ToDoDetailTableViewController: UITableViewController {
     @IBOutlet weak var saveBarButton: UIBarButtonItem!
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var noteView: UITextView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     @IBAction func cancelButtonPressed(_ sender: UIBarButtonItem) {
+        let isPresentingInAddMode = presentingViewController is UINavigationController
+        if isPresentingInAddMode {
+            dismiss(animated: true, completion: nil)
+        } else {
+            navigationController?.popViewController(animated: true)
+        }
     }
 }
